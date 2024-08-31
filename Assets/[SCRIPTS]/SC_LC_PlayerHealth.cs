260428c13c3f7c2fc521aec8cdec384b5d147fbd
LouisGD <louiscastillon352@gmail.com>
2024-08-31 13:16:06 +0200

    Louis - Micro correction barre de vie

--- Assets/[SCRIPTS]/SC_LC_PlayerHealth.cs
+++ Assets/[SCRIPTS]/SC_LC_PlayerHealth.cs
@@ -10,14 +10,15 @@ public class SC_LC_PlayerHealth : MonoBehaviour
 	SC_LC_PlayerGlobal player;
 
 	public Slider healthBar;
-    public TMP_Text healthDisplay;
+    public TMP_Text currentHealthDisplay;
+    public TMP_Text maxHealthDisplay;
 
 	[Space]
     public float maxHealth;
 	[Space]
 	public float currentHealth;
-	public float previousHealth;
-	public float targetHealth;
+	float previousHealth;
+	float targetHealth;
 	[Space]
 	public AnimationCurve healthSpeedCurve;
     float elapsedTime;
@@ -50,7 +51,8 @@ public class SC_LC_PlayerHealth : MonoBehaviour
 		currentHealth = Mathf.Clamp(currentHealth, 0f, maxHealth); //Clamps the current health of the player between 0 and the maxHealth value
 		targetHealth = Mathf.Clamp(targetHealth, 0f, maxHealth); //Clamps the target health of the player between 0 and the maxHealth value
 
-		healthDisplay.text = currentHealth.ToString("F0") + " / " + maxHealth.ToString("F0"); //Displays the values of the current health and the maximum health of the player on the health bar
+		currentHealthDisplay.text = currentHealth.ToString("F0"); //Displays current health value of the player on the health bar
+		maxHealthDisplay.text = maxHealth.ToString("F0"); //Displays maximum health value of the player on the health bar
 
 		currentHealth = Mathf.Lerp(previousHealth, targetHealth, curveValue); //Smoothly moves the current health value based on the animation curve
 
